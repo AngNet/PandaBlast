690ec7868f75a3c51aaadfc92103230caddfa830
Belinda <bywong@csu.fullerton.edu>
2018-05-13 15:48:57 -0700

    made it so userid would be anonymous instead of not having one.  doesn't work with whether or not a user is logged in or not yet

--- app/controllers/createthread.js
+++ app/controllers/createthread.js
@@ -5,23 +5,28 @@ export default Controller.extend({
   actions: {
     postThread: function() {
       var threadTitle = this.get('threadtitle');
-      var uid = dpd.users.me(function(me) {
-        return me;
-      })
-      //var uid = this.get('userId');
+      var uid;
       var threadTimestamp = new Date().getTime();
       var threadDescription = this.get('threadDescription');
-      alert('Thread was added.');
+      dpd.users.me(function(user) {
+        if (user)
+          uid = user.username;
+        if (uid == undefined)
+          uid = 'anonymous';
+        console.log(uid);
 
-      dpd.threads.post({
-        "threadTitle": threadTitle,
-        "userId": uid,
-        "threadTimestamp": threadTimestamp,
-        "threadDescription": threadDescription
-      }), function(result, err) {
-        if (err) return console.log(err);
-        console.log(result, result.id);
-      }
+        dpd.threads.post({
+            "threadTitle": threadTitle,
+            "userId": uid,
+            "threadTimestamp": threadTimestamp,
+            "threadDescription": threadDescription
+          }),
+          function(result, err) {
+            if (err) return console.log(err);
+            console.log(result, result.id);
+          }
+      })
+      alert("Thread added.");
     }
   }
 });
