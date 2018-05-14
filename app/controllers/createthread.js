8ad9fc91a3c6b44c45b9d9ec014355550a1528e5
csuf-bywong <bywong@csu.fullerton.edu>
2018-05-13 19:46:53 -0700

    Add files via upload
    couldn't push from command line, so i'm editing by hand

---
+++ app/controllers/createthread.js
@@ -0,0 +1,31 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+
+  actions: {
+    postThread: function() {
+      var threadTitle = this.get('threadtitle');
+      var uid;
+      var threadTimestamp = new Date().getTime();
+      var threadDescription = this.get('threadDescription');
+      dpd.users.me(function(user) {
+        if (user)
+          uid = user.username;
+        if (uid == undefined)
+          uid = 'anonymous';
+        console.log(uid);
+
+        dpd.threads.post({
+            "threadTitle": threadTitle,
+            "userId": uid,
+            "threadTimestamp": threadTimestamp,
+            "threadDescription": threadDescription
+          }),
+          function(result, err) {
+            if (err) return console.log(err);
+            console.log(result, result.id);
+          }
+      })
+    }
+  }
+});
