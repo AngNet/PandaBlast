d40f6d5a3408e55e8b110291bbb95c423e8d7bdb
bernarddo <36251176+bernarddo@users.noreply.github.com>
2018-05-14 15:39:41 -0700

    fixed conflicts

--- app/router.js
+++ app/router.js
@@ -7,22 +7,18 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-<<<<<<< HEAD
   this.route('login');
   this.route('register');
   this.route('account');
   this.route('threadlist');
   this.route('createthread');
   this.route('topic');
-=======
   this.route('pandabb', function(){
     this.route('pandabb', {path:  ':pandabb_id'})
   });
   this.route('users');
   this.route('authenticated');
-  this.route('login');
-  this.route('register');
->>>>>>> logins
+  
 });
 
 export default Router;
